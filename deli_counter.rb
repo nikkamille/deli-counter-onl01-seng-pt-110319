5a6531725d6e205f7adc9e65b5711117b2fc470f
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-29 23:56:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,6 @@
 katz_deli = []
+other_deli = ["Logan", "Avi", "Spencer"]
+another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(customer) 
   katz_deli == 0 
