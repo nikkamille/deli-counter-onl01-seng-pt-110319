612dfe9e7a4c612f0586f4a383330cd13d4f3f33
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-30 00:14:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,4 @@
-# katz_deli = []
+katz_deli = []
 # other_deli = ["Logan", "Avi", "Spencer"]
 # another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
