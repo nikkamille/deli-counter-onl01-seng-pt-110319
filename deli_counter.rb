27a2476cc6368706351a2aa7056b99c561659b6b
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-30 00:15:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 # another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(katz_deli)
-  while katz_deli == 0 
+  if katz_deli == 0 
     puts "The line is currently empty."
     #count += 1 
     #puts "The line is currently #{name}"
