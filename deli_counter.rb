7e2319e6a4f11975cc3a20c17e616cec514f6fca
Kamille Longalong <k.pagibigan@gmail.com>
2019-11-30 00:16:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 # other_deli = ["Logan", "Avi", "Spencer"]
 # another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
-def line(katz_deli)
+def line(customer)
   if katz_deli == 0 
     puts "The line is currently empty."
     #count += 1 
